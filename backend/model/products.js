68d9a9befe5454c9905a0e4bc9ab1d9b824349f0
Sayuru Dev <mailtosayuru@gmail.com>
2025-09-07 02:03:23 -0400

    mongo connected

--- backend/model/products.js
+++ backend/model/products.js
@@ -1,3 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect ("mongodb://127.0.0.1:27017/myShop")
+
+const productSchema = new mongoose.Schema({
+  id: Number,
+  name: String
+});
+
+const Product = mongoose.model("Product", productSchema);
+
 const products = [
   { id: 1, name: "Dior Sauvage" },
   { id: 2, name: "Bleu de Chanel" },
@@ -5,5 +17,9 @@ const products = [
   { id: 4, name: "Tom Ford Noir" },
   { id: 5, name: "Creed Aventus" }
 ];
+
+Product.insertMany(products)
+  .then(() => console.log("Products inserted"))
+  .catch((err) => console.error(err));
 
 export default products;
