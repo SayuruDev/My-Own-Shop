08bc067e43c3371f2040a1e298fd1b7e140d9297
Sayuru Dev <mailtosayuru@gmail.com>
2025-09-08 02:32:03 -0400

    register added

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import {button} from 'react'
+import React, { useState } from "react";
 
 function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = () => {
-        
+  const handleSubmit = () => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
 
-    }
-    return (
-        <div>
-        <div>RegisterPage</div>
-        <button className="registerSubmit" onClick={handleSubmit}>Submit</button>
-
-
-        </div>
-    )
+  return (
+    <div>
+      <h2>Register Page</h2>
+      <div>
+        <label>
+          Username:{" "}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter username"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+          />
+        </label>
+      </div>
+      <button className="registerSubmit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default RegisterPage;
